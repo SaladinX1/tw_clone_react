dac8d654bdb4401fd1af43b4525c14caef43375b
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-05-31 14:05:17 +0200

    Mise en place des routes avec react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 
+import React from "react";
 import './App.css';
 import Header from '../src/components/Header/Header'
 import Sidebar from '../src/components/Sidebar/Sidebar'
 import Games from './components/Games/Games';
+import TopStreams from './components/TopStreams/TopStreams';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+//import dotenv from 'dotenv';
+
+//dotenv.config();
 
 function App() {
   return (
+
+    <Router>
+
+
     <div className="App">
       <Header />
       <Sidebar />
-      <Games />
+      <Routes>
+        <Route path="/" exact element={<Games/>} />
+        <Route path="/top-streams" exact element={<TopStreams/>} />
+        </Routes>
     </div>
+
+
+    </Router>
   );
 }
 
